b055ba4dae7239ef132cef4f46078d298e0604a9
Annika Hägglund <annika.a.a.hagglund@outlook.com>
2022-05-18 18:45:13 +0200

    changes to Header and App

--- Skyltshoppen/src/App.jsx
+++ Skyltshoppen/src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-
-
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  
-
   return (
     <div className="App">
-      <Header/>
-   
+      <Header />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Skyltshoppen/src/components/Header.jsx
+++ Skyltshoppen/src/components/Header.jsx
@@ -1,38 +1,32 @@
-import { useState } from 'react'
-import '../App.css'
-import { Container, Nav, Navbar,  } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { useState } from "react";
+import "../App.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import Sign from '../../Public/img/SigniFy_.png'
-
-
+import Sign from "../../Public/img/SigniFy_.png";
 
 function Header() {
-
-
-  
-
   return (
     <div>
-
-<Navbar className='navbarColor'  expand="lg">
-  <Container>
-    <Navbar.Brand  href="#home"><img src={Sign} alt="logo"/>
-    </Navbar.Brand> 
-    <Navbar.Toggle   aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse  id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link bg="light"  href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-      
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-   
+      <Navbar className="navbarColor" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={Sign} alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link bg="light" href="#home">
+                Home
+              </Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
